2c2532b0764b5b8a1226da9d69d0226ec4a53c80
VidhiNegi19 <vidhinegi14@gmail.com>
2025-01-31 13:23:48 +0530

    day01.py

---
+++ day01.py
@@ -0,0 +1,122 @@
+#store number in variable
+x = 8
+print(x)
+y = 4.5
+
+#store number in variable
+x = 8
+print(x)
+y = 4.5
+print(y)
+
+#Addition
+z = x+y
+print(z)
+
+#subtraction
+z = x-y
+print(z)
+
+#find datatype
+print(type(z))
+
+#multiplication
+z = x*y
+print(z)
+
+#division
+z = x/y
+print(z)
+
+#floor division
+z = x//y 
+print(z)
+
+boolen_variable = False 
+print(type(boolen_variable))
+print(boolen_variable)
+
+
+#Equal
+s = (5 == 7)
+print(s)
+t = (4 == 4)
+print(t)
+
+#Notequal
+u = (7 != 6)
+print(u)
+v = (5 != 5)
+print(v)
+
+#Greater than 
+a = 4
+b = 6
+print(a<b)
+#less than
+a = 8
+b = 9
+print(a<b)
+
+#Greater than or equal to 
+print(3 >= 2)
+
+#less than or equal to 
+print(8 <= 9)
+print(y)
+
+#Addition
+z = x+y
+print(z)
+
+#subtraction
+z = x-y
+print(z)
+
+#find datatype
+print(type(z))
+
+#multiplication
+z = x*y
+print(z)
+
+#division
+z = x/y
+print(z)
+
+#floor division
+z = x//y 
+print(z)
+
+boolen_variable = False 
+print(type(boolen_variable))
+print(boolen_variable)
+
+
+#Equal
+s = (5 == 7)
+print(s)
+t = (4 == 4)
+print(t)
+
+#Notequal
+u = (7 != 6)
+print(u)
+v = (5 != 5)
+print(v)
+
+#Greater than 
+a = 4
+b = 6
+print(a<b)
+
+#less than
+a = 8
+b = 9
+print(a<b)
+
+#Greater than or equal to 
+print(3 >= 2)
+
+#less than or eqaul to 
+print(8 <= 9)
